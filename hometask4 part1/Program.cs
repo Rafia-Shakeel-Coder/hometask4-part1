4483d04da7b1ba78ca4222aeeaadb450d56cbf1f
Admin <Admin@DESKTOP-C1SCM0D>
2025-03-04 11:58:40 +0500

    Add project files.

---
+++ hometask4 part1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask4_part1
+{
+   
+        public class Book
+        {
+       
+            public String title;
+            public String author;
+            public String isbn;
+            public double price;
+
+        
+  public void Displaybookinfo()
+        {
+            Console.WriteLine("TITLE {0}",title);
+            Console.WriteLine("AUTHOR {0}",author);
+            Console.WriteLine("ISBN {0}",isbn);
+            Console.WriteLine("PRICE {0}",price);
+        }
+         public void ApplyDiscount(double percentage)
+        {
+            double discountAmount = price * (percentage / 100);
+            price -= discountAmount;
+            Console.WriteLine("AFTER APPLYING DISCOUNT {0},{1}",price,discountAmount);
+        }
+
+
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.title = "THE ATOMIC HABITS ";
+            book.author = "JAMES CLEAR";
+            book.isbn = "987627";
+            book.price = 2500;
+            //call the functions in main
+            Console.WriteLine("BOOK INFORMATION");
+            book.Displaybookinfo();
+            Console.WriteLine("APPLIED DISCOUNT OF 10% on book");
+            book.ApplyDiscount(10);
+            Console.WriteLine("UPDATED INFORMATION OF BOOK AFTER APPLYING DISCOUNT ");
+            book.Displaybookinfo();
+
+        }
+    }
+}
---
+++ hometask4 qno 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask4_qno_2
+{
+    public class Employee
+    {
+        public int employeeid { get; set; }
+        public string employeename { get; set; }
+        public string employeedepartment { get; set; }
+        public double employeesalary { get; set; }
+        public Employee(int id, string name, string department, double salary)
+        {
+            employeeid = id;
+            employeename = name;
+            employeedepartment = department;
+            employeesalary = salary;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("IDENTITY OF EMPLOYEE {0}", employeeid);
+            Console.WriteLine("NAME OF EMPLOYEE {0}", employeename);
+            Console.WriteLine("DEPARTMENT OF EMPLOYEE {0}", employeedepartment);
+            Console.WriteLine("SALARY OF EMPLOYEE {0}", employeesalary);
+
+        }
+
+        public void IncreaseSalary(double amount)
+        {
+            employeesalary += amount;
+            Console.WriteLine("SALARY AFTER INCREASING {0}", employeesalary);
+
+        }
+    }
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(76,"Rafia","Computer Science",90000);
+            employee.employeeid = 76;
+            employee.employeename = "RAFIA";
+            employee.employeedepartment = "COMPUTER SCIENCE";
+            employee.employeesalary = 900000;
+            Console.WriteLine("EMPLOYEE DETAILS ");
+           employee.DisplayEmployeeDetails();
+            Console.WriteLine("employee salary increase ");
+           employee.IncreaseSalary(10000);
+            Console.WriteLine("UPDATED INFORMATION OF EMPLOYEE ");
+            employee.DisplayEmployeeDetails();
+
+        }
+    }
+}
